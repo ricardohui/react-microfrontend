1abed4d6956534a6ac5aba0c639fe4f67e9effc3
Ricardo Hui <rcdhui@gmail.com>
2021-01-06 20:20:21 +0800

    add cart project

--- prod/ecomm/cart/webpack.config.js
+++ prod/ecomm/cart/webpack.config.js
@@ -3,16 +3,13 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 module.exports = {
   mode: "development",
   devServer: {
-    port: 8080,
+    port: 8082,
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "container",
-      remotes: {
-        products: "products@http://localhost:8081/remoteEntry.js",
-        cart: "cart@http://localhost:8082/remoteEntry.js",
-      },
-      // remoteEntry.js is the entry file for this Remote.
+      name: "cart",
+      filename: "remoteEntry.js",
+      exposes: { "./CartShow": "./src/index.js" },
     }),
     new HtmlWebpackPlugin({ template: "./public/index.html" }),
   ],
--- prod/ecomm/container/src/bootstrap.js
+++ prod/ecomm/container/src/bootstrap.js
@@ -1,4 +1,5 @@
 import "products/ProductsIndex";
+import "cart/CartShow";
 // products is what define in webpack.config.js
 // ProductsIndex is what defined in the webpack.config.js of product project
 console.log("container");
